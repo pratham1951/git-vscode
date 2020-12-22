023ce82ae3ab4c4e4b6c6e094e9bd2d0b4ed2fc5
Pratham <sharmapratham1951@gmail.com>
2020-12-22 15:42:10 +0530

    working area

---
+++ C++/test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter value"<<endl;
+    cin>>a;
+    cout<<"enter value"<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"output :"<<c<<endl;
+return 0;
+}
